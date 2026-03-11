e9383ad2ba11293964b4e041cc569c5c48f7a44c
GaracLui <158767416+GaracLui@users.noreply.github.com>
2026-03-11 17:34:42 -0300

    feat: JavaScript pipeline

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,16 @@ export default defineConfig({
         // Ehere Vite will output the built files
         // This should match the STATIC_ROOT in your Django settings.py
         outDir: path.resolve(__dirname, './static'),
-        emptyOutDir: false, // Don't delete existing files in the output directory
-        manifest: "manifest.json", // Generate a manifest file for Django to read
-        rollupOptions: (
-            inpot: {
+        emptyOutDir: false,             // Don't delete existing files in the output directory
+        manifest: "manifest.json",      // Generate a manifest file for Django to read
+        rollupOptions: {
+            input: {
                 'index': path.resolve(__dirname, './assets/index.js'),
             },
             output: {
                 // Output JS bundle with a name that includes the entry point name
                 entryFileNames: `js/[name]-bundle.js`,
             },
-        ),
+        },
     },
 });
