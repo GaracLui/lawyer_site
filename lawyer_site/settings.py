a2bd3a15e68261466e796f008106fd1d1bcc68fc
GaracLui <158767416+GaracLui@users.noreply.github.com>
2026-03-12 09:56:47 -0300

    feat: add django-vite package and configure installed apps

--- lawyer_site/settings.py
+++ lawyer_site/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_vite',
+    'about',
+    'blog',
+    'contact',
+    'core',
+    'service',
+
 ]
 
 MIDDLEWARE = [
