11de5b824dfc8e4c0dc6d1bf121671e9bf0ab1f7
GaracLui <158767416+GaracLui@users.noreply.github.com>
2026-03-15 21:43:24 -0300

    test: create a view and edit it url pattern

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
+from .models import SiteSettings
 
 # Create your views here.
+def base(request):
+    site_settings = SiteSettings.objects.first()  # Get the singleton instance of SiteSettings
+    context = {
+        'site_settings': site_settings,
+    }
+    return render(request, 'core/base.html', context)
