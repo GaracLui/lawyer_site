499d550cfb240bbba393d999bad7c90e47eaf2f2
GaracLui <158767416+GaracLui@users.noreply.github.com>
2026-03-15 21:05:53 -0300

    feat: configuration of context_processors

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,2 +1,9 @@
 from .models import SiteSettings
 
+def global_settings(request):
+    """
+    Context processor para agregar configuraciones globales a todas las plantillas.
+    """
+    return {
+        'settings': SiteSettings.objects.first()  # Asumiendo que solo hay una instancia de configuración
+    }
