913228d7053405c60e3c9f46f095d5152eb10d21
GaracLui <158767416+GaracLui@users.noreply.github.com>
2026-03-18 18:00:51 -0300

    refactor: base JavaScript to configure the icons to be used

---
+++ assets/javascript/icons.js
@@ -0,0 +1,31 @@
+export const ICONS = {
+    // Redes sociales 
+    instagram:  `<svg viewBox="0 0 16 16" fill="currentColor" class="">
+                    <path d="" />
+                </svg>`,
+    facebook:   `<svg viewBox="0 0 16 16" fill="currentColor" class="">
+                    <path d="" />
+                </svg>`,
+    whatsapp:   `<svg viewBox="0 0 16 16" fill="currentColor" class="">
+                    <path d="" />
+                </svg>`,
+
+    // Menu hamburgesa
+
+    // Modo oscuro/claro
+    sun:    `<svg viewBox="0 0 16 16" fill="currentColor" class="">
+                <path d="" />
+            </svg>`,
+    moon:   `<svg viewBox="0 0 16 16" fill="currentColor" class="">
+                <path d="" />
+            </svg>`,
+
+    // Iconos de estrellas
+    starFill:   `<svg viewBox="0 0 16 16" fill="currentColor" class="">
+                    <path d="" />
+                </svg>`,
+    starHalf:   `<svg viewBox="0 0 16 16" fill="currentColor" class="">
+                    <path d="" />
+                </svg>`,
+
+}
