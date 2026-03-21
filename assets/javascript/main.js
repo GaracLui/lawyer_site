db89db9cce7aa2869a0b0332aba194c3a24f551a
GaracLui <158767416+GaracLui@users.noreply.github.com>
2026-03-21 15:49:27 -0300

    refactor: add DaisyUI and start to use Django 6.+ partials

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,6 @@
-import '../styles/style.css';
-import { THEME_CLASSES } from './theme';
+const mybutton = document.getElementById("btn-back-to-top");
 
-
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', () => {
+    window.scrollTo({top: 0, behavior: "smooth", });
+})
--- assets/javascript/theme.js
+++
@@ -1,6 +0,0 @@
-export const THEME_CLASSES = {
-     // Títulos y Párrafos
-    h1: "text-text-heading dark:text-white text-4xl font-bold mb-4",
-    body: "text-text-body dark:text-slate-300 leading-relaxed",
-    
-};
